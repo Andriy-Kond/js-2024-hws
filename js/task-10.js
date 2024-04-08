b122a3dd67363381ef2b8b5b294d3c77c972afa0
Andrii <andrii.kondratovich@gmail.com>
2024-04-08 15:56:16 +0300

    solve: task-10

--- js/task-10.js
+++ js/task-10.js
@@ -3,3 +3,109 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+// Завдання 10
+// Напиши скрипт створення і очищення колекції елементів. Користувач вводить кількість елементів в input і натискає кнопку Створити, після чого рендериться колекція. Натисненням на кнопку Очистити, колекція елементів очищається.
+
+// Створи функцію createBoxes(amount), яка приймає один параметр - число. Функція створює стільки <div>, скільки вказано в amount і додає їх у div#boxes.
+
+// Розміри найпершого <div> - 30px на 30px.
+// Кожен елемент після першого повинен бути ширшим і вищим від попереднього на 10px.
+// Всі елементи повинні мати випадковий колір фону у форматі HEX. Використовуй готову функцію getRandomHexColor для отримання кольору.
+
+// Створи функцію destroyBoxes(), яка очищає вміст div#boxes, у такий спосіб видаляючи всі створені елементи.
+
+// const refs = {
+//   input: document.querySelector("#controls input"),
+//   createBtn: document.querySelector("[data-create]"),
+//   destroyBtn: document.querySelector("[data-destroy]"),
+//   boxes: document.querySelector("#boxes"),
+// };
+
+// * Solve #1:
+// refs.createBtn.addEventListener("click", createBoxes);
+// refs.destroyBtn.addEventListener("click", destroyBoxes);
+
+// function createBoxes() {
+//   let string = "";
+//   for (let i = 0; i < Number(refs.input.value); i += 1) {
+//     string += `<div style="width: ${30 + i * 10}px; height: ${
+//       30 + i * 10
+//     }px; background-color:${getRandomHexColor()}"></div>`;
+//   }
+
+//   refs.boxes.insertAdjacentHTML("afterbegin", string);
+//   // refs.boxes.innerHTML(string);
+// }
+
+// function destroyBoxes() {
+//   refs.boxes.innerHTML = "";
+//   refs.input.value = "";
+// }
+
+// * Solve #2:
+const refs = {
+  input: document.querySelector("#controls input"),
+  createBtn: document.querySelector("[data-create]"),
+  destroyBtn: document.querySelector("[data-destroy]"),
+  boxes: document.querySelector("#boxes"),
+};
+
+refs.createBtn.addEventListener("click", onCreateBtnClick);
+refs.destroyBtn.addEventListener("click", onDestroyBtnClick);
+
+function onCreateBtnClick() {
+  if (Number(refs.input.value) <= 0) {
+    refs.boxes.innerHTML = "";
+    const message = document.createElement("span");
+    message.textContent = "Введіть число більше нуля";
+    refs.boxes.append(message);
+    return;
+  }
+
+  createBoxes(Number(refs.input.value));
+
+  clearInput();
+}
+
+function createBoxes(amount) {
+  const elements = createElements(amount);
+  createStyles(elements);
+  const newElements = elements.map(el => el.outerHTML).join("");
+  // let newElements = "";
+  // elements.forEach(el => (newElements += el.outerHTML));
+
+  // const newElements = elements.reduce((previousValue, currentValue) => {
+  //   return (previousValue += `${currentValue}`);
+  // }, "");
+  // console.log("newElements >> newElements:::", newElements);
+
+  refs.boxes.insertAdjacentHTML("afterbegin", newElements);
+}
+
+function createElements(amount) {
+  const elements = [];
+
+  for (let i = 0; i < amount; i += 1) {
+    elements.push(document.createElement("div"));
+  }
+  return elements;
+}
+
+function createStyles(elements) {
+  return elements.map((el, idx) => {
+    el.style.width = `${30 + idx * 10}px`;
+    el.style.height = `${30 + idx * 10}px`;
+    el.style.backgroundColor = getRandomHexColor();
+    return el;
+  });
+}
+
+function onDestroyBtnClick() {
+  refs.boxes.innerHTML = "";
+  clearInput();
+}
+
+function clearInput() {
+  refs.input.value = "";
+}
